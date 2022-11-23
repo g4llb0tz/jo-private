7bca893b32b4da57487c1d4d8f13ad9f8ba9a873
g4llb0tz <117283526+g4llb0tz@users.noreply.github.com>
2022-11-23 17:21:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'THE GALL BOT'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6281229668980','60177426853','60177426853']
+global.premium = ['6281229668980']
+global.packname = 'The Gal Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
